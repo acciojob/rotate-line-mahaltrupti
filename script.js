9dd4cba1704adc7bb6ddefeaef2bff5d98d1ee94
mahaltrupti <truptimahal2004@gmail.com>
2025-10-07 21:34:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+// Select the line element
+const line = document.getElementById('line');
+
+// Initialize rotation angle
+let angle = 0;
+
+// Function to rotate the line continuously
+setInterval(() => {
+  angle += 2; // Increase angle by 2 degrees
+  line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`; 
+}, 20);
